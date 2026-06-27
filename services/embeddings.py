4bb395968b4928dfacaa6b817366bc93f30de500
exoshivam <meetskymau@gmail.com>
2026-06-27 13:37:54 +0530

    Fix Render deployment issue

--- services/embeddings.py
+++ services/embeddings.py
@@ -1,16 +1,16 @@
-from sentence_transformers import SentenceTransformer
-
 from config import Config
 
 
 class EmbeddingService:
 
     def __init__(self):
-        self.model = SentenceTransformer(
-            Config.EMBEDDING_MODEL
-        )
+        self.model = None
 
     def encode(self, texts):
+        if self.model is None:
+            from sentence_transformers import SentenceTransformer
+            print("Lazy loading embedding model...")
+            self.model = SentenceTransformer(Config.EMBEDDING_MODEL)
 
         if isinstance(texts, str):
             texts = [texts]
